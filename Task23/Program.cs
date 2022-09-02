4ab2e51e6343af3b4f955313d9e89d6f3a5ff9a8
ELENA <battilena@gmail.com>
2022-09-02 15:01:15 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу,которая принимает на вход число и выдаёт таблицу кубов от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int count = 1;
+while(count <= n)
+{
+    Console.WriteLine(Math.Pow(count, 3));
+    count ++;
+}
---
+++ Task_21/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход координаты 2х точек
+//и находит расстояние между ними в 3D пространстве
+Console.Clear();
+Console.WriteLine("Input A(x, y)");
+
+Console.Write("x: ");
+int x1  = int.Parse(Console.ReadLine());
+Console.Write("y: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("x: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input B(x, y");
+Console.Write("y: ");
+int y2 = int.Parse(Console.ReadLine());
+Console.Write("x: ");
+int x3  = int.Parse(Console.ReadLine());
+Console.Write("y: ");
+int y3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - y1, 2) + (Math.Pow(x2 - y2, 2) + (Math.Pow(x3 =y2,2)))));
